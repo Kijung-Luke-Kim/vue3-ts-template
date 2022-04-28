9c39b88efd0d49928b1122950ad6d96ea444501b
Kijung Luke Kim <rlwjd1504@gmail.com>
2022-04-28 22:20:58 +0900

    view 분리

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <img
+      alt="Vue logo"
+      src="../assets/logo.png"
+    />
+    <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  </div>
+</template>
+<script>
+import HelloWorld from "../components/HelloWorld.vue";
+export default {
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+<style lang="">
+</style>

e4eee35f7e920ae314d032bf4402357cb1873188
Luke(김기정) <kkj@gabia.com>
2022-05-09 10:55:37 +0900

    feat: Sentry 의존성 추가

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,23 @@ import App from "./App.vue";
 import router from "./router";
 import { store, key } from "./store";
 import axios from "axios";
+import * as Sentry from "@sentry/vue";
+import { BrowserTracing } from "@sentry/tracing";
 
 const app = createApp(App);
+
+Sentry.init({
+  app,
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  integrations: [
+    new BrowserTracing({
+      routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+      tracingOrigins: ["localhost", /^\//],
+    }),
+  ],
+  tracesSampleRate: 1.0,
+});
+
 app.use(router);
 app.use(store, key);
 app.config.globalProperties.axios = axios;
